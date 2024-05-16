14b5f995490c7c9774de66d469f9bc23aba559d0
GhifariArsa <s3893099@student.rmit.edu.au>
2024-05-16 22:55:24 +1000

    added skills section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./Components/Skills/Skills.js";
 import Me from "./Components/Intro/Me.js";
 import TopHeader from "./Components/TopHeader/TopHeader.js";
 
@@ -7,6 +8,13 @@ function App() {
     <div className="App">
       <TopHeader />
       <Me />
+      <div className="tech">
+        <span>
+          <h1>Technical Skills</h1>
+        </span>
+      </div>
+      <About />
+      <About />
     </div>
   );
 }
